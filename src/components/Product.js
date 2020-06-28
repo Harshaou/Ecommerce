46440b37344b7bc5d0f8646fe17a4a7de935c480
Harshad <harsh.haou@gmail.com>
2020-06-28 14:16:23 +0530

    ui-set

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 import {ProductConsumer} from '../context'
 
 class Product extends Component {
 
     render() {
         const {id,title,img,price,inCart} = this.props.product
-        console.log(img)
         return (
             <ProductWrapper className='col-9 mx-auto  col-md-6 col-lg-3 my-3'>
-                <h1>ss</h1>
                 <div className="card">
                     <div onClick={() => console.log('clicked')} className="img-container p-5">
                         <Link to='/details'>
                             <img className='card-img-top' src={img} alt="product"/>
                         </Link>
+                        <button className='cart-btn' disabled={inCart? true: false }>
+                          {inCart? ( <p className='text-capitalize mb-0' disabled>inCart</p> ) : ( <i className='fas fa-cart-plus'></i> ) }
+                        </button>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                      <p className="align-self-center mb-0">
+                        {title}
+                      </p>
+                      <h5 className="text-blue font-italic mb-0">
+                        <span className="mr-1">$</span>
+                        {price}
+                      </h5>
                     </div>
                 </div>
             </ProductWrapper>
         );
     }
+}
+
+Product.prototypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    image: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool
+  }).isRequired
 }
 
 const ProductWrapper = styled.div`
